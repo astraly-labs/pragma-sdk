793b81e0ca2ff9e515565be72b6396671d02c93f
Evolve <artevolve@yahoo.com>
2023-09-01 17:08:25 -0700

    ♻️ move back root path to tests/

--- pragma/core/abis/abi.py
+++ pragma/core/abis/abi.py
@@ -4,28 +4,6 @@ from pathlib import Path
 
 from starknet_py.common import create_sierra_compiled_contract
 
-
-def find_repo_root(start_directory: Path) -> Path:
-    """Finds the root directory of the repo by walking up the directory tree
-    and looking for a known file at the repo root.
-    """
-    current_directory = start_directory
-    while current_directory != current_directory.parent:  # Stop at filesystem root
-        if (current_directory / "pyproject.toml").is_file():
-            return current_directory
-        current_directory = current_directory.parent
-    raise ValueError("Repository root not found!")
-
-
-current_file_directory = Path(__file__).parent
-repo_root = find_repo_root(current_file_directory)
-
-SUBMODULE_DIR = repo_root / "pragma-oracle"
-MOCK_DIR = repo_root / "pragma/tests" / "mock"
-
-CONTRACTS_COMPILED_DIR = SUBMODULE_DIR / "target/dev"
-MOCK_COMPILED_DIR = MOCK_DIR / "compiled_contracts"
-
 BUILD_DIR = Path(os.path.dirname(__file__))
 
 
