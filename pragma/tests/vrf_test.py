dcd80d3bc40f8197cb578612d5f31cb4201294f5
Evolve <artevolve@yahoo.com>
2023-11-22 12:38:39 +0100

    ✅ more tests

--- pragma/tests/vrf_test.py
+++ pragma/tests/vrf_test.py
@@ -5,6 +5,7 @@ from urllib.parse import urlparse
 import pytest
 import pytest_asyncio
 from starknet_py.contract import Contract, DeclareResult, DeployResult
+from starknet_py.transaction_errors import TransactionRevertedError
 from starknet_py.net.account.account import Account
 
 from pragma.core.client import PragmaClient
@@ -113,7 +114,11 @@ async def test_client_setup(vrf_pragma_client: PragmaClient, account: Account):
 
 
 @pytest.mark.asyncio
-async def test_randomness_mixin(vrf_pragma_client: PragmaClient, randomness_contracts: (Contract, Contract), address_and_private_key):
+async def test_randomness_mixin(
+    vrf_pragma_client: PragmaClient,
+    randomness_contracts: (Contract, Contract),
+    address_and_private_key,
+):
     _, private_key = address_and_private_key
     (randomness, example_randomness) = randomness_contracts
 
@@ -124,7 +129,9 @@ async def test_randomness_mixin(vrf_pragma_client: PragmaClient, randomness_cont
     num_words = 1
     caller_address = vrf_pragma_client.account_address()
 
-    await vrf_pragma_client.request_random(seed, callback_address, callback_gas_limit, publish_delay, num_words)
+    await vrf_pragma_client.request_random(
+        seed, callback_address, callback_gas_limit, publish_delay, num_words
+    )
     pending_reqs = await vrf_pragma_client.get_pending_requests(caller_address)
     assert pending_reqs == [0]
 
@@ -133,4 +140,88 @@ async def test_randomness_mixin(vrf_pragma_client: PragmaClient, randomness_cont
     assert pending_reqs == []
 
     status = await vrf_pragma_client.get_request_status(caller_address, 0)
-    assert status.variant == 'FULFILLED'
+    assert status.variant == "FULFILLED"
+
+    # Request cancellation test
+    seed = 2
+    await vrf_pragma_client.request_random(
+        seed, callback_address, callback_gas_limit, publish_delay, num_words
+    )
+    pending_reqs = await vrf_pragma_client.get_pending_requests(caller_address)
+    assert pending_reqs == [1]
+    status = await vrf_pragma_client.get_request_status(caller_address, 1)
+    assert status.variant == "RECEIVED"
+    await vrf_pragma_client.cancel_random_request(
+        pending_reqs[0],
+        caller_address,
+        seed,
+        callback_address,
+        callback_gas_limit,
+        publish_delay,
+        num_words,
+    )
+    pending_reqs = await vrf_pragma_client.get_pending_requests(caller_address)
+    assert pending_reqs == []
+    status = await vrf_pragma_client.get_request_status(caller_address, 1)
+    assert status.variant == "CANCELLED"
+
+    # Request cancellation failed if request is already fulfilled
+    seed = 3
+    await vrf_pragma_client.request_random(
+        seed, callback_address, callback_gas_limit, publish_delay, num_words
+    )
+    pending_reqs = await vrf_pragma_client.get_pending_requests(caller_address)
+    assert pending_reqs == [2]
+    status = await vrf_pragma_client.get_request_status(caller_address, 2)
+    assert status.variant == "RECEIVED"
+    await vrf_pragma_client.handle_random(int(private_key, 16), min_block=0)
+    pending_reqs = await vrf_pragma_client.get_pending_requests(caller_address)
+    assert pending_reqs == []
+    status = await vrf_pragma_client.get_request_status(caller_address, 2)
+    assert status.variant == "FULFILLED"
+
+    # try:
+    #     await vrf_pragma_client.cancel_random_request(
+    #         2,
+    #         caller_address,
+    #         seed,
+    #         callback_address,
+    #         callback_gas_limit,
+    #         publish_delay,
+    #         num_words,
+    #     )
+    #     assert False
+    # except TransactionRevertedError as err:
+    #     err_msg = "Execution was reverted; failure reason: [0x7265717565737420616c72656164792066756c66696c6c6564]"
+    #     if not err_msg in err.message:
+    #         raise err
+
+
+@pytest.mark.asyncio
+async def test_fails_gas_limit(
+    vrf_pragma_client: PragmaClient,
+    randomness_contracts: (Contract, Contract),
+    address_and_private_key,
+):
+    _, private_key = address_and_private_key
+    (randomness, example_randomness) = randomness_contracts
+
+    seed = 1
+    callback_gas_limit = 10
+    callback_address = example_randomness.address
+    publish_delay = 0
+    num_words = 1
+    caller_address = vrf_pragma_client.account_address()
+
+    await vrf_pragma_client.request_random(
+        seed, callback_address, callback_gas_limit, publish_delay, num_words
+    )
+    pending_reqs = await vrf_pragma_client.get_pending_requests(caller_address)
+    assert pending_reqs == [0]
+
+    await vrf_pragma_client.handle_random(int(private_key, 16), min_block=0)
+    pending_reqs = await vrf_pragma_client.get_pending_requests(caller_address)
+    assert pending_reqs == []
+
+    status = await vrf_pragma_client.get_request_status(caller_address, 0)
+    assert status.variant == "OUTOFGAS"
