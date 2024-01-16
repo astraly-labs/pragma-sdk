737a946664b184b2162251e760ac94f8b9911397
JordyRo1 <jordy.romu@orange.fr>
2024-01-16 22:26:30 +0100

    jediswap pool retrieval

--- pragma/publisher/fetchers/starknetamm.py
+++ pragma/publisher/fetchers/starknetamm.py
@@ -62,10 +62,10 @@ class PoolKey:
         return f"PoolKey({self.token_0}, {self.token_1}, {self.fee}, {self.tick_spacing}, {self.extension})"
 
 class StarknetAMMFetcher: 
-    client = get_client_from_network("testnet")
-    EKUBO_PUBLIC_API: str = 'https://goerli-api.ekubo.org'
+    client = get_client_from_network("mainnet")
+    EKUBO_PUBLIC_API: str = 'https://mainnet-api.ekubo.org'
     EKUBO_CORE_CONTRACT: str = '0x031e8a7ab6a6a556548ac85cbb8b5f56e8905696e9f13e9a858142b8ee0cc221'
-    
+    JEDISWAP_ETH_USDC_POOL: str = '0x04d0390b777b424e43839cd1e744799f3de6c176c7e32c1812a41dbd9c19db6a'
     # Parameters for the pool 
 
     # These are the parameters of the most used pool in Ekubo for the testing pair ETH/USDC
@@ -94,7 +94,7 @@ class StarknetAMMFetcher:
             return f"Error: {e}"
 
 
-    async def on_fetch_ekubo_price(self) -> Union[float, PublisherFetchError]:
+    async def on_fetch_ekubo_price(self) -> float:
         token_0, token_1 = min(self.ETH_ADDRESS, self.USDC_ADDRESS), max(self.ETH_ADDRESS, self.USDC_ADDRESS)
         fee = math.floor(self.POOL_FEE * 2**128)
 
@@ -103,29 +103,40 @@ class StarknetAMMFetcher:
         # Since the tick spacing is represented as an exponent of TICK_BASE, we can use the logarithm to find the number of tick
         tick = round(math.log(1 + self.TICK_SPACING)/math.log(self.TICK_BASE))
         pool_key = PoolKey(int(token_0, 16), int(token_1, 16), int(fee), int(tick),int(self.POOL_EXTENSION))
-        print(pool_key.serialize())
         call = Call(
             to_addr=self.EKUBO_CORE_CONTRACT,
             selector=get_selector_from_name("get_pool_price"),
             calldata=pool_key.serialize(),
         )
         pool_info= await self.client.call_contract(call)
-        print(pool_info)
-        sqrt_ratio = pool_info['sqrt_ratio']
+        sqrt_ratio = pool_info[0]
+        assert sqrt_ratio != 0, " sqrt_ratio is null"
         return (sqrt_ratio/2**128)**2 * 10*(self.ETH_DECIMALS - self.USDC_DECIMALS)
 
-        
+    
+    async def on_fetch_jedi_price(self) -> float:
+        call = Call(
+            to_addr=self.JEDISWAP_ETH_USDC_POOL,
+            selector=get_selector_from_name("get_reserves"),
+            calldata=[],
+        )
+        reserves_infos = await self.client.call_contract(call)
+        token_0_reserve = reserves_infos[0] + reserves_infos[1] * 2**128
+        token_1_reserve = reserves_infos[2] + reserves_infos[3] * 2**128
+        return token_1_reserve/token_0_reserve * 10**(self.ETH_DECIMALS - self.USDC_DECIMALS)
+
 
     async def fetch_strk_price(self) -> Union[float, PublisherFetchError]:
         ekubo_price = await self.off_fetch_ekubo_price()
-        print(ekubo_price)
+        jedi_swap_price = await self.on_fetch_jedi_price()
+        # TODO: operation with the elements
 
 
 
 async def main():
-
     fetcher = StarknetAMMFetcher()
-    price = await fetcher.on_fetch_ekubo_price()
+    price = await fetcher.on_fetch_jedi_price()
+    print(price)
 
 # Run the main function in the asyncio event loop
 asyncio.run(main())
