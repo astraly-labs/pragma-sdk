2ff57b74ea947cf8b4bb5e1c6abcf9144dc344df
JordyRo1 <jordy.romu@orange.fr>
2024-02-21 19:31:15 +0100

    Refactors publishers

--- pragma/publisher/fetchers/bybit.py
+++ pragma/publisher/fetchers/bybit.py
@@ -30,36 +30,16 @@ class BybitFetcher(PublisherInterfaceT):
         pair = asset["pair"]
         if (pair == ("STRK", "USD")):
             pair = ("STRK", "USDT")
-        if (pair == ("ETH", "STRK")):
-            url = f"{self.BASE_URL}symbol=STRKUSDT"
-            async with session.get(url) as resp:
-                if resp.status == 404:
-                    return PublisherFetchError(
-                        f"No data found for {'/'.join(pair)} from Bybit"
-                    )
-                result = await resp.json()
-                if result["retCode"] == 10001:
-                    return PublisherFetchError(
-                        f"No data found for {'/'.join(pair)} from Bybit"
-                    )
-                eth_url = f"{self.BASE_URL}symbol=ETHUSDT"
-                eth_resp = await session.get(eth_url)
-                eth_result = await eth_resp.json()
-                return self._construct(asset, result, ((float(eth_result["result"]["list"][0]["bid1Price"]) + float(eth_result["result"]["list"][0]["ask1Price"])))/2)
-        else: 
-            url = f"{self.BASE_URL}symbol={pair[0]}{pair[1]}"
-            async with session.get(url) as resp:
-                if resp.status == 404:
-                    return PublisherFetchError(
-                        f"No data found for {'/'.join(pair)} from Bybit"
-                    )
-                result = await resp.json()
-                if result["retCode"] == 10001:
-                    return PublisherFetchError(
-                        f"No data found for {'/'.join(pair)} from Bybit"
-                    )
-
-                return self._construct(asset, result)
+        url = self.format_url(pair[0], pair[1])
+        async with session.get(url) as resp:
+            if resp.status == 404:
+                return PublisherFetchError(
+                    f"No data found for {'/'.join(pair)} from Bybit"
+                )
+            result = await resp.json()
+            if result["retCode"] == 10001:
+               return await self.operate_usdt_hop(asset, session)
+            return self._construct(asset, result)
 
     def _fetch_pair_sync(
         self, asset: PragmaSpotAsset
@@ -67,38 +47,17 @@ class BybitFetcher(PublisherInterfaceT):
         pair = asset["pair"]
         if (pair == ("STRK", "USD")):
             pair = ("STRK", "USDT")
-        if (pair ==("ETH", "STRK")):
-
-            url = f"{self.BASE_URL}symbol=STRKUSDT"
-            resp = requests.get(url)
-            if resp.status_code == 404:
-                return PublisherFetchError(
-                    f"No data found for {'/'.join(pair)} from Bybit"
-                )
-            result = resp.json()
-            if result["retCode"] == 10001:
-                return PublisherFetchError(
-                    f"No data found for {'/'.join(pair)} from Bybit"
-                )
-            eth_url = f"{self.BASE_URL}symbol=ETHUSDT"
-            eth_resp = requests.get(eth_url)
-            eth_result = eth_resp.json()
-            return self._construct(asset, result, ((float(eth_result["result"]["list"][0]["bid1Price"]) + float(eth_result["result"]["list"][0]["ask1Price"])))/2)
-        else: 
-            url = f"{self.BASE_URL}symbol={pair[0]}{pair[1]}"
-
-            resp = requests.get(url)
-            if resp.status_code == 404:
-                return PublisherFetchError(
-                    f"No data found for {'/'.join(pair)} from Bybit"
-                )
-            result = resp.json()
-            if result["retCode"] == 10001:
-                return PublisherFetchError(
-                    f"No data found for {'/'.join(pair)} from Bybit"
-                )
-
-            return self._construct(asset, result)
+        
+        url = self.format_url(pair[0], pair[1])
+        resp = requests.get(url)
+        if resp.status_code == 404:
+            return PublisherFetchError(
+                f"No data found for {'/'.join(pair)} from Bybit"
+            )
+        result = resp.json()
+        if result["retCode"] == 10001:
+            return self.operate_usdt_hop_sync(asset)
+        return self._construct(asset, result)
 
     async def fetch(
         self, session: ClientSession
@@ -121,27 +80,77 @@ class BybitFetcher(PublisherInterfaceT):
         return entries
 
     def format_url(self, quote_asset, base_asset):
-        url = f"{self.BASE_URL}?symbol={quote_asset}/{base_asset}"
+        url = f"{self.BASE_URL}symbol={quote_asset}{base_asset}"
         return url
-
-    def _construct(self, asset, result, eth_price = None) -> SpotEntry:
+    
+    async def operate_usdt_hop(self, asset, session) -> SpotEntry: 
         pair = asset["pair"]
-        data = result["result"]["list"]
+        url_pair1 = self.format_url(asset["pair"][0], "USDT")
+        async with session.get(url_pair1) as resp: 
+            if resp.status == 404:
+                return PublisherFetchError(
+                    f"No data found for {'/'.join(pair)} from Bybit - hop failed for {pair[0]}"
+                )
+            pair1_usdt = await resp.json()
+            if pair1_usdt["retCode"] == 10001:
+                return PublisherFetchError(
+                    f"No data found for {'/'.join(pair)} from Bybit - hop failed for {pair[0]}"
+                )
+        url2 = self.format_url(asset['pair'][1], "USDT")
+        async with session.get(url2) as resp: 
+            if resp.status == 404:
+                return PublisherFetchError(
+                    f"No data found for {'/'.join(pair)} from Bybit - hop failed for {pair[1]}"
+                )
+            pair2_usdt = await resp.json()    
+            if pair2_usdt["retCode"] == 10001:
+                return PublisherFetchError(
+                    f"No data found for {'/'.join(pair)} from Bybit - hop failed for {pair[1]}"
+                )
+        return self._construct(asset, pair2_usdt, pair1_usdt)
+
+    def operate_usdt_hop_sync(self, asset) -> SpotEntry:
+        pair = asset["pair"]
+        url_pair1 = self.format_url(asset["pair"][0], "USDT")
+        resp = requests.get(url_pair1)
+        if resp.status_code == 404:
+            return PublisherFetchError(
+                f"No data found for {'/'.join(pair)} from Bybit - hop failed for {pair[0]}"
+            )
+        pair1_usdt = resp.json()
+        if pair1_usdt["retCode"] == 10001:
+            return PublisherFetchError(
+                f"No data found for {'/'.join(pair)} from Bybit - hop failed for {pair[0]}"
+            )
+        url_pair2 = self.format_url(asset['pair'][1], "USDT")
+        resp = requests.get(url_pair2)
+        if resp.status_code == 404:
+            return PublisherFetchError(
+                f"No data found for {'/'.join(pair)} from Bybit - hop failed for {pair[1]}"
+            )
+        pair2_usdt = resp.json()
+        if pair2_usdt["retCode"] == 10001:
+            return PublisherFetchError(
+                f"No data found for {'/'.join(pair)} from Bybit - hop failed for {pair[1]}"
+            )
+        return self._construct(asset, pair2_usdt, pair1_usdt)
+    
+    def _construct(self, asset, result, hop_result = None) -> SpotEntry:
+        pair = asset["pair"]
+        bid = float(result["result"]["list"][0]['bid1Price'])
+        ask = float(result["result"]["list"][0]["ask1Price"])
+        price = (bid + ask) / 2
+        if hop_result is not None: 
+            hop_bid = float(hop_result["result"]["list"][0]['bid1Price'])
+            hop_ask = float(hop_result["result"]["list"][0]["ask1Price"])
+            hop_price = (hop_bid + hop_ask) / 2
+            price = hop_price/price
         timestamp = int(time.time())
-        if pair == ("ETH", "STRK"):
-            ask = float(data[0]['ask1Price'])
-            bid = float(data[0]["bid1Price"])
-            price = (ask + bid) / 2.0
-            price_int = int((eth_price/price) * (10 ** asset["decimals"]))
-        else: 
-            ask = float(data[0]['ask1Price'])
-            bid = float(data[0]["bid1Price"])
-            price = (ask + bid) / 2.0
-            price_int = int(price * (10 ** asset["decimals"]))
-        
+        price_int = int(price * (10 ** asset["decimals"]))
         pair_id = currency_pair_to_pair_id(*pair)
-        volume = float(data[0]["volume24h"])
+        volume = float(result["result"]["list"][0]["volume24h"]) if hop_result is None else 0
         logger.info("Fetched price %d for %s from Bybit", price, "/".join(pair))
+
         return SpotEntry(
             pair_id=pair_id,
             price=price_int,
@@ -150,4 +159,5 @@ class BybitFetcher(PublisherInterfaceT):
             source=self.SOURCE,
             publisher=self.publisher,
         )
+
 
