f0e9f85920335215a23ef653fe14244065a7f3cf
devops72-sre <>
2024-03-30 13:18:28 +0530

    added ci

---
+++ stagecoach/jobs/publishers/offchain-publisher/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+aws s3 cp {{S3_SECRET_PATH}} /root/test/{{SERVICE_NAME}}
+# Authenticate Docker to your Amazon ECR
+aws ecr get-login-password --region {{region}} | docker login --username AWS --password-stdin {{aws_account_id}}.dkr.ecr.{{region}}.amazonaws.com
+
+# Pull the latest image
+docker pull {{docker_image_tag}}
+
+# # Stop the currently running container
+# docker stop {{SERVICE_NAME}}|| true
+
+# # Remove the stopped container
+# docker rm {{SERVICE_NAME}}|| true
+
+# # Run the new container
+# docker run --rm --name my-cron-job-container -d --log-driver=awslogs --log-opt awslogs-region={{region}} --log-opt awslogs-group={{aws_logs_group}} --env-file /root/env.list {{docker_image_tag}}
