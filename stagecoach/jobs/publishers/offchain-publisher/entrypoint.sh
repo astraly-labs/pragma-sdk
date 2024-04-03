36858be141e9555100421d59cce9873e3f76e3a4
devops72-sre <>
2024-04-03 21:05:10 +0530

    fixed error

--- stagecoach/jobs/publishers/offchain-publisher/entrypoint.sh
+++ stagecoach/jobs/publishers/offchain-publisher/entrypoint.sh
@@ -2,8 +2,8 @@
 # Export the environment variables to a file
 printenv > /etc/environment
 # Set up the cron job
-echo "* * * * * /bin/bash -c '. /etc/environment; /usr/local/bin/python /app.py > /proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/cronjob
-echo "* * * * * root (sleep 30 ; /bin/bash -c '. /etc/environment; /usr/local/bin/python /app.py  > /proc/1/fd/1 2>/proc/1/fd/2)" >> /etc/cron.d/cronjob
+echo "* * * * * root . /etc/environment; /usr/local/bin/python /app.py > /proc/1/fd/1 2>/proc/1/fd/2" > /etc/cron.d/cronjob
+echo "* * * * * root (sleep 30 ; . /etc/environment; /usr/local/bin/python /app.py  > /proc/1/fd/1 2>/proc/1/fd/2)" >> /etc/cron.d/cronjob
 chmod 0644 /etc/cron.d/cronjob
 
 # Start cron in the foreground
