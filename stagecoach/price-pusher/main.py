622df8a591c2d674889bf8c1ce610dc8e2719573
0xevolve <Artevolve@yahoo.com>
2024-06-18 17:11:37 +0100

    fix: refactor

--- stagecoach/price-pusher/main.py
+++ stagecoach/price-pusher/main.py
@@ -25,76 +25,57 @@ from pragma.publisher.future_fetchers import BinanceFutureFetcher, ByBitFutureFe
 
 logger = get_stream_logger()
 
-SECRET_NAME = os.environ.get("SECRET_NAME", None)
-SPOT_ASSETS = os.environ.get("SPOT_ASSETS", "BTC/USD")
-FUTURE_ASSETS = os.environ.get("FUTURE_ASSETS", "BTC/USDT")
-PUBLISHER = os.environ["PUBLISHER"]
-PUBLISHER_ADDRESS = int(os.environ.get("PUBLISHER_ADDRESS"), 16)
-PROPELLER_API_KEY = os.environ.get("PROPELLER_API_KEY")
-API_KEY = os.environ.get("API_KEY")
-PAGINATION = os.environ.get("PAGINATION")
-API_URL = os.environ.get("API_URL", "https://api.dev.pragma.build/node")
-if PAGINATION is not None:
-    PAGINATION = int(PAGINATION)
+
+def load_env_variables():
+    return {
+        "SECRET_NAME": os.environ.get("SECRET_NAME", None),
+        "SPOT_ASSETS": os.environ.get("SPOT_ASSETS", "BTC/USD"),
+        "FUTURE_ASSETS": os.environ.get("FUTURE_ASSETS", "BTC/USDT"),
+        "PUBLISHER": os.environ.get("PUBLISHER", "PRAGMA"),
+        "PUBLISHER_ADDRESS": int(os.environ.get("PUBLISHER_ADDRESS"), 16),
+        "PROPELLER_API_KEY": os.environ.get("PROPELLER_API_KEY"),
+        "API_KEY": os.environ.get("API_KEY"),
+        "PAGINATION": (
+            int(os.environ.get("PAGINATION", 0))
+            if os.environ.get("PAGINATION")
+            else None
+        ),
+        "API_URL": os.environ.get("API_URL", "https://api.dev.pragma.build/node"),
+    }
 
 
-async def fetch_and_publish():
-    spot_assets = [
-        get_spot_asset_spec_for_pair_id(asset)
-        for asset in SPOT_ASSETS.split(",")
-        if len(asset) > 0
+def get_asset_specs(assets_str, get_asset_spec_func):
+    return [
+        get_asset_spec_func(asset) for asset in assets_str.split(",") if len(asset) > 0
     ]
-    future_assets = [
-        get_future_asset_spec_for_pair_id(asset)
-        for asset in FUTURE_ASSETS.split(",")
-        if len(asset) > 0
-    ]
-    if len(spot_assets) == 0 and len(future_assets) == 0:
+
+
+async def fetch_and_publish(env_vars):
+    spot_assets = get_asset_specs(
+        env_vars["SPOT_ASSETS"], get_spot_asset_spec_for_pair_id
+    )
+    future_assets = get_asset_specs(
+        env_vars["FUTURE_ASSETS"], get_future_asset_spec_for_pair_id
+    )
+
+    if not spot_assets and not future_assets:
         logger.error(
             "No assets to publish. Check SPOT_ASSETS and FUTURE_ASSETS env variables."
         )
         return
 
-    # publisher_private_key = _get_pvt_key()
-    publisher_private_key = int(os.environ["PUBLISHER_PRIVATE_KEY"], 16)
+    # publisher_private_key = int(os.environ["PUBLISHER_PRIVATE_KEY"], 16)
+    publisher_private_key = _get_pvt_key(env_vars["SECRET_NAME"])
 
     publisher_client = PragmaAPIClient(
         account_private_key=publisher_private_key,
-        account_contract_address=PUBLISHER_ADDRESS,
-        api_base_url=API_URL,
-        api_key=API_KEY,
+        account_contract_address=env_vars["PUBLISHER_ADDRESS"],
+        api_base_url=env_vars["API_URL"],
+        api_key=env_vars["API_KEY"],
     )
 
     fetcher_client = PragmaPublisherClient()
-
-    fetcher_client.add_fetchers(
-        [
-            fetcher(spot_assets, PUBLISHER)
-            for fetcher in (
-                BitstampFetcher,
-                CexFetcher,
-                DefillamaFetcher,
-                OkxFetcher,
-                GeckoTerminalFetcher,
-                HuobiFetcher,
-                KucoinFetcher,
-                BybitFetcher,
-                BinanceFetcher,
-            )
-        ]
-    )
-    fetcher_client.add_fetcher(
-        PropellerFetcher(spot_assets, PUBLISHER, PROPELLER_API_KEY)
-    )
-    fetcher_client.add_fetchers(
-        [
-            fetcher(future_assets, PUBLISHER)
-            for fetcher in (
-                BinanceFutureFetcher,
-                ByBitFutureFetcher,
-            )
-        ]
-    )
+    add_fetchers(fetcher_client, spot_assets, env_vars["PUBLISHER"])
 
     entries = await fetcher_client.fetch()
     logger.info(f"Got {entries} entries")
@@ -102,30 +83,57 @@ async def fetch_and_publish():
     # logger.info(f"Successfully published data with response {response}")
 
 
-# def _get_pvt_key():
-#     region_name = "eu-west-3"
+def _get_pvt_key(secret_name):
+    region_name = "eu-west-3"
 
-#     # Create a Secrets Manager client
-#     session = boto3.session.Session()
-#     client = session.client(service_name="secretsmanager", region_name=region_name)
-#     get_secret_value_response = client.get_secret_value(SecretId=SECRET_NAME)
-#     return int(
-#         json.loads(get_secret_value_response["SecretString"])["PUBLISHER_PRIVATE_KEY"],
-#         16,
-#     )
+    # Create a Secrets Manager client
+    session = boto3.session.Session()
+    client = session.client(service_name="secretsmanager", region_name=region_name)
+    get_secret_value_response = client.get_secret_value(SecretId=secret_name)
+    return int(
+        json.loads(get_secret_value_response["SecretString"])["PUBLISHER_PRIVATE_KEY"],
+        16,
+    )
+
+
+def add_fetchers(fetcher_client, spot_assets, publisher):
+    fetchers = [
+        DefillamaFetcher,
+        # BitstampFetcher,
+        # CexFetcher,
+        # OkxFetcher,
+        # GeckoTerminalFetcher,
+        # HuobiFetcher,
+        # KucoinFetcher,
+        # BybitFetcher,
+        # BinanceFetcher,
+    ]
+    fetcher_client.add_fetchers(
+        [fetcher(spot_assets, publisher) for fetcher in fetchers]
+    )
+
+    # Uncomment to add PropellerFetcher
+    # fetcher_client.add_fetcher(PropellerFetcher(spot_assets, publisher, PROPELLER_API_KEY))
+
+    # Uncomment to add future fetchers
+    # future_fetchers = [
+    #     BinanceFutureFetcher,
+    #     ByBitFutureFetcher,
+    # ]
+    # fetcher_client.add_fetchers([fetcher(future_assets, publisher) for fetcher in future_fetchers])
 
 
 async def main():
+    env_vars = load_env_variables()
     while True:
-        start_time = time.time()  # Record the start time
+        start_time = time.time()
         try:
-            await fetch_and_publish()
+            await fetch_and_publish(env_vars)
         except Exception as e:
             logger.error(f"Error occurred: {e}")
-        end_time = time.time()  # Record the end time
-        duration = end_time - start_time  # Calculate the duration
+        end_time = time.time()
+        duration = end_time - start_time
         logger.info(f"fetch_and_publish took {duration:.2f} seconds")
-        # await asyncio.sleep(10)  # Run every 10 seconds
 
 
 if __name__ == "__main__":
