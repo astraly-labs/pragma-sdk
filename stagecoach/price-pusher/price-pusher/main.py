6dd1a6e8809e5c5cf6fe7c618b3cad0ea87ea0f0
Maxime CASEAUX <caseaux.maxime@gmail.com>
2024-06-21 17:58:31 +0200

    added some args

--- stagecoach/price-pusher/price-pusher/main.py
+++ stagecoach/price-pusher/price-pusher/main.py
@@ -2,10 +2,27 @@ import click
 import logging
 import re
 from .config import read_price_config_file
+from pragma.publisher.client import PragmaPublisherClient, PragmaAPIClient
+from typing import Union
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+
+def retrieve_key_from_aws(secret_name):
+    #todo
+    pass
+
+def get_private_key(args):
+    private_key_splitted = args.split(':')
+    if len(private_key_splitted) != 2:
+        raise click.BadParameter("private key parameter is not well formatted")
+    if private_key_splitted[0] == 'aws':
+        return retrieve_key_from_aws(private_key_splitted[1])
+    return private_key_splitted[1]
+
+
 
 @click.command()
 @click.option('--config-file', help='Path to config.yaml')
@@ -15,7 +32,8 @@ logger = logging.getLogger(__name__)
 @click.option('--publisher-name', help='Your publisher name')
 @click.option('--publisher-address', help='Your publisher address')
 @click.option('--api-key', default=None, help='pragma api key to publish offchain')
-def main(log_level, config_file, network, private_key, publisher_name, publisher_address, api_key):
+@click.option('--api-url', default=None, help='pragma api base url')
+def main(log_level, config_file, network, private_key, publisher_name, publisher_address, api_key, api_url):
     numeric_log_level = getattr(logging, log_level.upper(), None)
     if not isinstance(numeric_log_level, int):
         raise ValueError(f'Invalid log level: {log_level}')
@@ -28,6 +46,24 @@ def main(log_level, config_file, network, private_key, publisher_name, publisher
     
     if network and not re.match(r'^(onchain|offchain):(testnet|mainnet)$',network):
         raise click.BadParameter("Network must be in the format <onchain|offchain:testnet|mainnet>")
+    
+    target, net = network.split(':')
+
+    key = get_private_key(private_key)
+
+    publisher : Union[PragmaPublisherClient,PragmaAPIClient]
+    if target == "onchain":
+        publisher = PragmaPublisherClient(network=net, account_contract_address=publisher_address, account_private_key=key)
+    elif target == "offchain":
+        if api_key == None:
+            raise click.BadParameter("Argument api-key can't be None if offchain is selected")
+        if api_url == None:
+            raise click.BadParameter("Argument api-url can't be None if offchain is selected")
+        publisher = PragmaAPIClient(account_contract_address=publisher_address, account_private_key=private_key, api_key=api_key, api_base_url=api_url)
+    
+
+    # run_puller(target, net, keys, publisher)
+    # run_pusher(target, net, keys, publisher)
     # run
 
 
