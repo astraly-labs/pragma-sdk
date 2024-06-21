d3902466c9f958b88b9d7554dcba49b1a04088f2
Maxime CASEAUX <caseaux.maxime@gmail.com>
2024-06-21 17:09:27 +0200

    feat: interfaces

--- stagecoach/price-pusher/price-pusher/main.py
+++ stagecoach/price-pusher/price-pusher/main.py
@@ -1,19 +1,21 @@
 import click
 import logging
+import re
 from .config import read_price_config_file
 
 # Set up logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
 
-
 @click.command()
 @click.option('--config-file', help='Path to config.yaml')
 @click.option('--log-level', default='INFO', help='Set the logging level (DEBUG, INFO, WARNING, ERROR, CRITICAL)')
-def main(log_level, config_file):
-    # CLI
-    # Parse argv
-    # get log level
+@click.option('--network', help='<onchain|offchain:testnet|mainnet>')
+@click.option('--private-key', help='<aws|keystore:secret_name|path>')
+@click.option('--publisher-name', help='Your publisher name')
+@click.option('--publisher-address', help='Your publisher address')
+@click.option('--api-key', default=None, help='pragma api key to publish offchain')
+def main(log_level, config_file, network, private_key, publisher_name, publisher_address, api_key):
     numeric_log_level = getattr(logging, log_level.upper(), None)
     if not isinstance(numeric_log_level, int):
         raise ValueError(f'Invalid log level: {log_level}')
@@ -21,11 +23,11 @@ def main(log_level, config_file):
     logger.setLevel(numeric_log_level)
     logging.getLogger().setLevel(numeric_log_level)
     
-    # retrieve config from file
-
     config = read_price_config_file(config_file)
     print(config)
-
+    
+    if network and not re.match(r'^(onchain|offchain):(testnet|mainnet)$',network):
+        raise click.BadParameter("Network must be in the format <onchain|offchain:testnet|mainnet>")
     # run
 
 
--- stagecoach/price-pusher/price-pusher/types.py
+++ stagecoach/price-pusher/price-pusher/types.py
@@ -1,9 +1,12 @@
-from pragma.core.assets import PragmaSpotAsset, PragmaFutureAsset, get_asset_spec_for_pair_id_by_type
-from typing import List, Optional
+from pragma.core.assets import PragmaSpotAsset, PragmaFutureAsset, get_asset_spec_for_pair_id_by_type, PragmaAsset
+from pragma.core.entry import SpotEntry, FutureEntry, Entry
+from typing import List, Optional, Dict
 from typing_extensions import Annotated
 from enum import Enum
 
 from pydantic import BaseModel, field_validator, RootModel, Field, ConfigDict
+from abc import ABC, abstractmethod
+import asyncio
 
 class PriceConfig(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
@@ -54,3 +57,56 @@ class DataSource(Enum):
     ONCHAIN = 1
     OFFCHAIN = 2
     DEFILLAMA = 3
+
+UnixTimestamp = int
+DurationInSeconds = int
+
+class IPriceListener(ABC):
+    @abstractmethod
+    async def start(self) -> None:
+        pass
+
+    @abstractmethod
+    def get_latest_price_info(self, pair_id: str) -> Optional[Entry]:
+        pass
+
+
+class ChainPriceListener(IPriceListener, ABC):
+    def __init__(self, polling_frequency: DurationInSeconds, price_items: List[PragmaAsset]) -> None:
+        self.polling_frequency = polling_frequency
+        self.price_items = price_items
+        self.latest_price_info: Dict[str, Entry] = {}
+
+    async def start(self) -> None:
+        await self.poll_prices()
+        while True:
+            await asyncio.sleep(self.polling_frequency)
+            await self.poll_prices()
+
+    async def poll_prices(self) -> None:
+        for item in self.price_items:
+            current_price_info = await self.get_on_chain_price_info(item.id)
+            if current_price_info:
+                self.update_latest_price_info(item.id, current_price_info)
+
+    def update_latest_price_info(self, pair_id: str, observed_price: Entry) -> None:
+        cached_latest_price_info: Optional[Entry] = self.get_latest_price_info(pair_id)
+
+        if cached_latest_price_info and cached_latest_price_info.get_timestamp() > observed_price.get_timestamp():
+            return
+
+        self.latest_price_info[pair_id] = observed_price
+
+    def get_latest_price_info(self, pair_id: str) -> Optional[Entry]:
+        return self.latest_price_info.get(pair_id)
+
+    @abstractmethod
+    async def get_on_chain_price_info(self, pair_id: str) -> Optional[Entry]:
+        pass
+
+
+class IPricePusher(ABC):
+    @abstractmethod
+    async def update_price_feed(self, pair_ids: List[str], pub_times_to_push: List[UnixTimestamp]) -> None:
+        pass
+
