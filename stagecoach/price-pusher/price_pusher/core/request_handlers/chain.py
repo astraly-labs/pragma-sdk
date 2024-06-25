f1c8b3961ffedfb5d16c2e7ef60c9f5c128e5896
akhercha <akherchache@pm.me>
2024-06-25 20:24:33 +0200

    feat(price-pusher):

--- stagecoach/price-pusher/price_pusher/core/request_handlers/chain.py
+++ stagecoach/price-pusher/price_pusher/core/request_handlers/chain.py
@@ -8,6 +8,7 @@ from pragma.publisher.client import PragmaOnChainClient
 
 from price_pusher.core.request_handlers.interface import IRequestHandler
 from price_pusher.utils.assets import asset_to_pair_id
+from price_pusher.utils.retries import retry_async
 
 logger = logging.getLogger(__name__)
 
@@ -27,17 +28,21 @@ class ChainRequestHandler(IRequestHandler):
 
     async def _fetch_oracle_price(self, asset: PragmaAsset) -> Optional[Entry]:
         pair_id = asset_to_pair_id(asset)
-        oracle_response = None
-        try:
+
+        async def fetch_action():
             if asset["type"] == "SPOT":
                 oracle_response = await self.client.get_spot(pair_id)
                 return SpotEntry.from_oracle_response(asset, oracle_response)
             elif asset["type"] == "FUTURE":
                 oracle_response = await self.client.get_future(pair_id, 0)
                 return FutureEntry.from_oracle_response(asset, oracle_response)
-        except Exception as e:
-            raise Exception(
-                f"Could not fetch fetch price for {asset_to_pair_id(asset)}: {e}"
-            )
-        if oracle_response is None:
             return None
+
+        try:
+            return await fetch_action()
+        except Exception:
+            try:
+                logger.warning(f"🤔 Fetching price for {pair_id} failed. Retrying...")
+                return await retry_async(fetch_action, retries=5, delay_in_s=5)
+            except Exception as e:
+                raise ValueError(f"Retries for fetching {pair_id} still failed: {e}")
