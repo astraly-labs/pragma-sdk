a3be30b5ff8e54a9604af973031233f5bfb58b63
0xevolve <Artevolve@yahoo.com>
2024-07-03 13:16:02 +0200

    Update api_client_test.py

--- sdk/pragma/tests/api_client_test.py
+++ sdk/pragma/tests/api_client_test.py
@@ -12,16 +12,16 @@ from aioresponses import aioresponses
 from starknet_py.net.client import Client
 
 from pragma.onchain.client import PragmaOnChainClient
-from pragma.common.types import RPC_URLS
-from pragma.publisher.client import PragmaAPIClient, PragmaAPIError
-from pragma.tests.constants import MOCK_DIR, SAMPLE_ASSETS
+from pragma.onchain.constants import RPC_URLS
+from pragma.offchain.client import PragmaAPIClient
+from pragma.offchain.exceptions import PragmaAPIError
+from pragma.tests.constants import MOCK_DIR, USD_PAIRS
 from pragma.tests.fixtures.devnet import get_available_port
 
 JEDISWAP_POOL = "0x4e021092841c1b01907f42e7058f97e5a22056e605dce08a22868606ad675e0"
 
 ACCOUNT_ADDRESS = os.getenv("TESTNET_ACCOUNT_ADDRESS")
 ACCOUNT_PRIVATE_KEY = os.getenv("TESTNET_PRIVATE_KEY")
-# %% SPOT
 
 
 @pytest.fixture(scope="module")
