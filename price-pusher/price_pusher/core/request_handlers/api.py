32713ae268b34db1f0c373577b759b00fc053dd2
mcaseaux <mcaseaux@student.42.fr>
2024-07-05 15:32:16 +0200

    add again future

--- price-pusher/price_pusher/core/request_handlers/api.py
+++ price-pusher/price_pusher/core/request_handlers/api.py
@@ -3,7 +3,7 @@ from typing import Optional
 
 from pragma_sdk.common.types.types import DataTypes
 from pragma_sdk.common.types.pair import Pair
-from pragma_sdk.common.types.entry import Entry, SpotEntry
+from pragma_sdk.common.types.entry import Entry, FutureEntry, SpotEntry
 from pragma_sdk.offchain.client import PragmaAPIClient, EntryResult
 from pragma_sdk.common.types.types import AggregationMode
 from pragma_sdk.offchain.types import Interval
@@ -33,18 +33,27 @@ class APIRequestHandler(IRequestHandler):
                 interval=Interval.ONE_MINUTE,
                 aggregation=AggregationMode.MEDIAN,
             )
+            entry = FutureEntry(
+                pair_id=entry_result.pair_id,
+                price=int(entry_result.data, 16),
+                # PragmaAPI returns timestamp in millis, we convert to s
+                timestamp=entry_result.timestamp / 1000,
+                source=PRAGMA_API_SOURCE_NAME,
+                publisher=PRAGMA_API_PUBLISHER_NAME,
+                expiry_timestamp=entry_result.expiry
+            )
         else:
             entry_result: EntryResult = await self.client.get_entry(
                 pair=pair.__repr__(),
                 interval=Interval.ONE_MINUTE,
                 aggregation=AggregationMode.MEDIAN,
             )
-        entry = SpotEntry(
-            pair_id=entry_result.pair_id,
-            price=int(entry_result.data, 16),
-            # PragmaAPI returns timestamp in millis, we convert to s
-            timestamp=entry_result.timestamp / 1000,
-            source=PRAGMA_API_SOURCE_NAME,
-            publisher=PRAGMA_API_PUBLISHER_NAME,
-        )
+            entry = SpotEntry(
+                pair_id=entry_result.pair_id,
+                price=int(entry_result.data, 16),
+                # PragmaAPI returns timestamp in millis, we convert to s
+                timestamp=entry_result.timestamp / 1000,
+                source=PRAGMA_API_SOURCE_NAME,
+                publisher=PRAGMA_API_PUBLISHER_NAME,
+            )
         return entry
