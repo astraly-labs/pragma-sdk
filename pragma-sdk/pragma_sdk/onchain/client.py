09a5514610ec391fbfbde105cdd47ba0c339957d
akhercha <akherchache@pm.me>
2024-07-25 13:24:45 +0200

    feat(merkle_maker): Returning Any 😹😹

--- pragma-sdk/pragma_sdk/onchain/client.py
+++ pragma-sdk/pragma_sdk/onchain/client.py
@@ -1,5 +1,5 @@
 import logging
-from typing import List, Optional, Union
+from typing import List, Optional, Union, Any
 
 from starknet_py.net.account.account import Account
 from starknet_py.net.full_node_client import FullNodeClient
@@ -168,7 +168,7 @@ class PragmaOnChainClient(  # type: ignore[misc]
         )
         return await client.get_balance(token_address)  # type: ignore[no-any-return]
 
-    async def get_block_number(self) -> int:
+    async def get_block_number(self) -> Any:
         """Returns the current block number."""
         return await self.full_node_client.get_block_number()
 
