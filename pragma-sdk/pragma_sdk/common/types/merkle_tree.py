98c42691b2a2b1bc7fa3d37fd37ab6db5bc337ee
akhercha <adel@pragma.build>
2024-07-26 13:06:19 +0200

    feat(deribit_fetcher): Failing tests uhh'

--- pragma-sdk/pragma_sdk/common/types/merkle_tree.py
+++ pragma-sdk/pragma_sdk/common/types/merkle_tree.py
@@ -1,4 +1,4 @@
-from typing import List, Any
+from typing import List
 from starknet_py.utils.merkle_tree import MerkleTree as StarknetPyMT
 
 
@@ -6,11 +6,8 @@ MerkleProof = List[int]
 
 
 class MerkleTree(StarknetPyMT):
-    def get_proof(self, target: Any) -> MerkleProof:
+    def get_proof(self, target: int) -> MerkleProof:
         """Generate a Merkle proof for the given target."""
-        if not isinstance(target, int):
-            target = hash(target)
-
         if target not in self.leaves:
             raise ValueError("Target not found in the Merkle tree")
 
@@ -26,21 +23,22 @@ class MerkleTree(StarknetPyMT):
 
         return proof
 
-    def verify_proof(self, target: Any, proof: MerkleProof) -> Any:
+    def verify_proof(self, target: int, proof: MerkleProof) -> bool:
         """Verify a Merkle proof."""
-        if not isinstance(target, int):
-            target = hash(target)
+        if target not in self.leaves:
+            return False
 
         leaf_index = self.leaves.index(target)
+        current_hash = target
 
-        for i, sibling_hash in enumerate(proof):
-            is_left = (leaf_index // (2**i)) % 2 == 0
-            if is_left:
-                target = self.hash_method.hash(target, sibling_hash)
+        for level, sibling_hash in enumerate(proof):
+            if leaf_index % 2 == 0:
+                current_hash = self.hash_method.hash(current_hash, sibling_hash)
             else:
-                target = self.hash_method.hash(sibling_hash, target)
+                current_hash = self.hash_method.hash(sibling_hash, current_hash)
+            leaf_index //= 2
 
-        return target == self.root_hash
+        return current_hash == self.root_hash
 
     def as_dict(self) -> dict:
         return {
