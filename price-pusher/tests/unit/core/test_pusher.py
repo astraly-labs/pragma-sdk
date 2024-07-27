7829be83a499f26263eb1f8b472390b8033f2105
akhercha <akherchache@pm.me>
2024-07-27 12:47:33 +0200

    fix(price_pusher_improvements): lint

--- price-pusher/tests/unit/core/test_pusher.py
+++ price-pusher/tests/unit/core/test_pusher.py
@@ -44,7 +44,7 @@ async def test_update_price_feeds_success(caplog):
         for record in caplog.records
     )
     assert price_pusher.consecutive_push_error == 0
-    assert price_pusher.is_publishing_on_chain == False
+    assert not price_pusher.is_publishing_on_chain
 
 
 @pytest.mark.asyncio
