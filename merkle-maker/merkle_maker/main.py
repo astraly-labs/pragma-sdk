9a633973e536ead0595f1a912f86195eeb2566bc
akhercha <akherchache@pm.me>
2024-08-05 14:59:58 +0200

    feat(merkle_feed_mixin): Devnet option

--- merkle-maker/merkle_maker/main.py
+++ merkle-maker/merkle_maker/main.py
@@ -83,7 +83,7 @@ async def main(
     required=True,
     default="sepolia",
     type=click.Choice(
-        ["sepolia", "mainnet"],
+        ["sepolia", "mainnet", "devnet"],
         case_sensitive=False,
     ),
     help="On which networks the checkpoints will be set.",
