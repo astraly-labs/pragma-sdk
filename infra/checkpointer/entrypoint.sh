59b71ec68613b9c5f0d73729463fe56ae33f2a12
devops72-sre <>
2024-10-01 00:30:37 +0530

    removed logging

--- infra/checkpointer/entrypoint.sh
+++ infra/checkpointer/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -euox pipefail
+set -euo pipefail
 export INFISICAL_TOKEN=$(infisical login --method=universal-auth --client-id=${INFISICAL_CLIENT_ID} --client-secret=${INFISICAL_CLIENT_SECRET} --silent --plain)
 infisical export  --projectId=${INFISICAL_PROJECT_ID} --env=${INFISICAL_ENV}  --path=${INFISICAL_APP_PATH} > .env
 source .env
