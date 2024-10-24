b95f5207e903746e68d38fbb456265ca720c3a89
akhercha <akherchache@pm.me>
2024-10-24 12:38:05 +0200

    feat(ekubo_fetcher): Error test

--- pragma-sdk/tests/integration/fetchers/rpc_fetchers_test.py
+++ pragma-sdk/tests/integration/fetchers/rpc_fetchers_test.py
@@ -2,6 +2,9 @@ import pytest
 
 from unittest import mock
 
+from pragma_sdk.common.types.pair import Pair
+from pragma_sdk.common.exceptions import PublisherFetchError
+
 from tests.integration.fixtures.fetchers import get_mock_data
 from tests.integration.constants import (
     ONCHAIN_SAMPLE_PAIRS,
@@ -33,3 +36,28 @@ async def test_async_rpc_fetcher(rpc_fetcher_config):
     ):
         result = await fetcher.fetch(session=mock.MagicMock())
         assert are_entries_list_equal(result, rpc_fetcher_config["expected_result"])
+
+
+@mock.patch("time.time", mock.MagicMock(return_value=12345))
+@pytest.mark.asyncio
+async def test_publisher_error_async_rpc_fetcher(rpc_fetcher_config):
+    pairs = [Pair.from_tickers("SOL", "USD")]
+    fetcher = rpc_fetcher_config["fetcher_class"](pairs, PUBLISHER_NAME)
+
+    with (
+        mock.patch.object(
+            fetcher.client,
+            "get_spot",
+            return_value=STABLE_MOCK_PRICE,
+        ),
+        mock.patch.object(
+            fetcher.client.full_node_client,
+            "call_contract",
+            return_value=[1, 0],
+        ),
+    ):
+        result = await fetcher.fetch(session=mock.MagicMock())
+        expected = [
+            PublisherFetchError("Price feed not initialized for SOL/USDC in Ekubo")
+        ]
+        assert result == expected
