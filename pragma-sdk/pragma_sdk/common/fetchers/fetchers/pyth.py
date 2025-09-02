8792e20a9ed521d34a55c47e9e95565768657be5
0xevolve <Artevolve@yahoo.com>
2025-09-02 16:31:15 +0200

    chore: bump 2.8.10
    - add usdt/usd to pyth fetcher

--- pragma-sdk/pragma_sdk/common/fetchers/fetchers/pyth.py
+++ pragma-sdk/pragma_sdk/common/fetchers/fetchers/pyth.py
@@ -15,6 +15,7 @@ logger = get_pragma_sdk_logger()
 PYTH_FEED_IDS: Dict[str, str] = {
     # Major cryptocurrencies
     "BTC/USD": "0xe62df6c8b4a85fe1a67db44dc12de5db330f7ac66b72dc658afedf0f4a415b43",
+    "USDT/USD": "0x2b89b9dc8fdf9f34709a5b106b472f0f39bb6ca9ce04b0fd7f2e971688e2e53b",
     "ETH/USD": "0xff61491a931112ddf1bd8147cd1b641375f79f5825126d665480874634fd0ace",
     "SOL/USD": "0xef0d8b6fda2ceba41da15d4095d1da392a0d2f8ed0c6c7bc0f4cfac8c280b56d",
     "BNB/USD": "0x2f95862b045670cd22bee3114c39763a4a08beeb663b145d283c31d7d1101c4f",
